7d006141acd20b5c6cbba1589e2a2c72cfc15a27
Ibrahim Nehme <ibrahimnehme42@gmail.com>
2024-08-12 16:12:46 +0300

    final push

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: 'https://github.com/mribrahim314/task-manager-application',
+  base: '/task-manager-application/',
   plugins: [react()],
   optimizeDeps: {
     include: ['@mui/x-date-pickers', 'date-fns'],
